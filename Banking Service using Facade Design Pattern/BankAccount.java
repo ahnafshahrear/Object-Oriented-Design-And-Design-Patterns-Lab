6ced42281c149b60435b6ecc809fa7453f0c1eac
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-01-23 15:47:48 +0600

    Account added

--- Banking Service using Facade Design Pattern/BankAccount.java
+++
@@ -1,9 +0,0 @@
-public interface BankAccount {
-    public void deposit(int amount);
-
-    public boolean withdraw(int amount);
-
-    public String getAccountNumber();
-
-    public int getBalance();
-}
