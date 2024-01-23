d28bff3a3ce7f509d99a7bf3aeae4f809e2ccc62
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-01-23 16:21:48 +0600

    SavingAccount added

--- Banking Service using Facade Design Pattern/SavingAccount.java
+++ Banking Service using Facade Design Pattern/SavingAccount.java
@@ -1,40 +1,38 @@
-public class SavingAccount {
-    public class ChequingAccount implements Account {
-        private int amount, accountNumber;
-    
-        ChequingAccount(int amount)
-        {
-            this.amount = amount;
-            this.accountNumber = (int)(Math.random() * 100);
-            System.out.println("Chequing account successfully created");
-        }
-    
-        @Override
-        public void deposit(int amount) {
-            this.amount += amount;
-            System.out.println("Successfully deposited $" + amount);
-        }
-    
-        @Override
-        public void withdraw(int amount) {
-            this.amount -= amount;
-            System.out.println("Successfully withdrawn $" + amount);
-        }
-    
-        @Override
-        public void transfer(int amount, Account to) {
-            this.withdraw(amount);
-            to.deposit(amount);
-        }
-    
-        @Override
-        public int getAccountNumber() {
-            return accountNumber;
-        }
-    
-        public int getAmount()
-        {
-            return amount;
-        }
-    }    
+public class SavingAccount implements Account {
+    private int amount, accountNumber;
+
+    SavingAccount(int amount)
+    {
+        this.amount = amount;
+        this.accountNumber = (int)(Math.random() * 100);
+        System.out.println("Saving account successfully created");
+    }
+
+    @Override
+    public void deposit(int amount) {
+        this.amount += amount;
+        System.out.println("Successfully deposited $" + amount);
+    }
+
+    @Override
+    public void withdraw(int amount) {
+        this.amount -= amount;
+        System.out.println("Successfully withdrawn $" + amount);
+    }
+
+    @Override
+    public void transfer(int amount, Account to) {
+        this.withdraw(amount);
+        to.deposit(amount);
+    }
+
+    @Override
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public int getAmount()
+    {
+        return amount;
+    }
 }
